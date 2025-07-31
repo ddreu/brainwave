a4bdd239e3cc4745f0b37e2af892a0bfc5ca5ff6
Andrew <3922017@holycross.edu.ph>
2025-07-31 16:03:31 +0800

    pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
 
 const App = () => {
   return (
@@ -12,6 +15,9 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
       </div>
       <ButtonGradient />
     </>

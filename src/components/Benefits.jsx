a449eb3a57751923f92a3cde54ef019ff52cc1a4
Andrew <3922017@holycross.edu.ph>
2026-01-31 14:41:12 +0800

    bug fix

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -2,7 +2,7 @@ import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
-import { GradientLight } from "./design/benefits";
+import { GradientLight } from "./design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
